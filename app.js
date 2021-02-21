9411c70a09cc89d125c4506af36a809cd9e272b8
andrewwmclain <mclaina1@spu.edu>
2021-02-20 18:37:24 -0800

    initial files

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var session = require('express-session'); //express session
+var bodyParser = require('body-parser');
+var app = express();
+
+//connect to db
+var db = require('./database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
+var messagesRouter = require('./routes/messages');
+var postsRouter = require('./routes/posts');
+var homepageRouter = require('./routes/homepage');
+var myprofileRouter = require('./routes/myprofile');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +28,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
+app.use('/posts', postsRouter);
+app.use('/homepage', homepageRouter);
+app.use('/myprofile', myprofileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +53,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
