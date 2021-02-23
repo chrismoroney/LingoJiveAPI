b3a81771d05b7ffcc5efc626bf1e4fe2e762ee61
Christopher Moroney <christopher.moroney1@gmail.com>
2021-02-23 00:14:14 -0800

    update all js files from other repo

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 // For the Data Model
-let PostSchema = require('../models/Post.js');
+let Post = require('../models/Post.js');
 
 
 function HandleError(response, reason, message, code){
@@ -10,34 +10,19 @@ function HandleError(response, reason, message, code){
 }
 
 router.get('/', (req, res) => {
-    PostSchema.find({},(err, posts)=> {
-        // res.sendFile('posts.html', {root: 'views'});
-        res.send(posts);
+    Post.find({},(err, posts)=> {
+        res.sendFile('posts.html', {root: 'views'});
+        // res.send(posts);
     })
 })
 
 router.post('/', (req, res) => {
-    let newPost = JSON.parse(JSON.stringify(req.body));
-    let post = new PostSchema({
-        Name: newPost.Name,
-        Body: newPost.Body,
-    });
-    post.save((error) => {
-        if (error){
-            response.send({"error": error});
-        }else{
-            PostSchema.find({},(err, posts)=> {
-                // res.sendFile('posts.html', {root: 'views'});
-                res.send(posts);
-            })
-        }
-    });
-    // let post = new Post(req.body);
-    // post.save((err) =>{
-    //     if(err)
-    //         sendStatus(500);
-    //     res.sendStatus(200);
-    // })
+    let post = new Post(req.body);
+    post.save((err) =>{
+        if(err)
+            sendStatus(500);
+        res.sendStatus(200);
+    })
 })
 
 module.exports = router;
