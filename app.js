afebd5a5fea4abcc74ec9d02c55864d6fd458745
Chris <christopher.moroney1@gmail.com>
2021-05-16 03:11:59 -0700

    Should be fixed now for repo

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/profilepics', express.static('profilepics'));
+app.use(express.static('profilepics'));
+//app.use('/profilepics', express.static('profilepics'));
 
 
 app.use(bodyParser.json());
